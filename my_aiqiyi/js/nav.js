850f39699e66a0fe5e615fe545ecc4b807da2cbd
cbjbbcb <654892860@qq.com>
2017-10-04 21:02:29 +0800

    mayu

---
+++ my_aiqiyi/js/nav.js
@@ -0,0 +1,10 @@
+$(function() {
+			onscroll = function() {
+				var top = $("body").scrollTop();
+				if(top > 490) {
+					$(".fix").fadeIn("fast");
+				} else {
+					$(".fix").fadeOut("fast");
+				}
+			}
+	}	)
